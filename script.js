9855d5b2081a7e79e2ff1b2b594c8e44e61524d8
Ganchenko-Ilya <108551053+Ganchenko-Ilya@users.noreply.github.com>
2022-08-23 03:20:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,238 @@
+// Задание 2
+
+// Есть объект с именами и заработными платами инженеров:
+// const engineers = {
+// Den: 1000,
+// Matt: 5000,
+// Steve: 2000
+// }
+// Вывести в консоль через шаблонные строки заработную плату всех работников в таком формате:
+
+// Заработная плата ххх составляет ххх рублей.
+
+const engineers = {
+    names: ['Den','Matt','Steve'],
+    moneys:[1000, 5000, 2000]
+
+}
+
+for(let i = 0; i < engineers.names.length; i++){
+
+console.log(`Заработная плата ${engineers.names[i]} составляет ${engineers.moneys[i]} рублей `);
+
+}
+
+// Задание 3
+
+// Создать массив из 5 элементов.
+
+// Используя цикл for, вывести каждый второй элемент массива в консоль.
+
+let numbers3 = [1,2,3,4,5];
+for(let i = 1; i < numbers3.length; i +=2){
+console.log(numbers3[i]);
+}
+
+// Задание 4
+
+// Есть массив произвольных чисел:
+// let numbers = [42, 65, 49, 68, 56, 47, 70, 42, 51, 35, 58, 63, 40, 70]
+
+// Вывести в консоль значения всех элементов массива и соответствующие им индексы в таком виде:
+
+// Индексу 0 соответствует число 42
+// Индексу 1 соответствует число 65
+// и т.д.
+
+let numbers4 = [42, 65, 49, 68, 56, 47, 70, 42, 51, 35, 58, 63, 40, 70];
+for(let i = 0; i < numbers4.length; i++){
+console.log(`Индексу ${i} соответствует число ${numbers4[i]}`);
+}
+
+// Задание 5
+
+// Дан массив объектов, например:
+
+// let questions = [{
+//     question: "What's the currency of the USA?",
+//     choices: ["US dollar", "Ruble", "Horses", "Gold"],
+//     corAnswer: 0
+// }, {
+//     question: "Where was the American Declaration of Independence signed?",
+//     choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+//     corAnswer: 0
+// }];
+
+// Добавить в каждый объект дополнительное поле usersAnswer со значением null.
+
+// Решение должно работать для массива любой длины.
+
+let questions = [{
+    question: "What's the currency of the USA?",
+    choices: ["US dollar", "Ruble", "Horses", "Gold"],
+    corAnswer: 0
+},
+{
+    question: "Where was the American Declaration of Independence signed?",
+    choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+    corAnswer: 0
+}];
+for(let i = 0; i < questions.length; i++){
+questions[i].usersAnswer = null;
+
+}
+console.log(questions);
+
+// Задание 6
+
+// Есть массив произвольных чисел:
+// let numbers = [42, 65, 49, 68, 56, 47, 70, 42, 51, 35, 58, 63, 40, 70]
+
+// 1) Посчитать и вывести в консоль сумму элементов в массиве.
+
+// Подсказка: в задачах такого типа нужно заранее предусмотреть переменную (например, sum) и записать в нее начальное значение (в случае с суммой - ноль). Затем уже описать цикл, в котором эта сумма будет считаться (в каждой итерации цикла к ней будет прибавляться следующий элемент массива).
+
+// 2) Посчитать и вывести в консоль сумму четных элементов в массиве.
+
+// 3) Найти и вывести в консоль максимальное число массива.
+
+// Подсказка: как и в случае с суммой, здесь сначала нужно объявить переменную (например, max), записать в нее первый элемент массива, а затем уже в цикле искать максимальное значение.
+
+// 4) Определить и вывести в консоль индекс максимального числа массива (или индексы, если число встречается более одного раза). Само максимальное число мы уже нашли в прошлой части задачи, повторно его искать не нужно.
+
+//Задание 6.1
+let numbers6 = [42, 65, 49, 68, 56, 47, 70, 42, 51, 35, 58, 63, 40, 70];
+
+const numberSum = (array) => {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
+};
+console.log(numberSum(numbers6));
+
+//задание 6.2
+const numberSumEven = (arrayEven) => {
+  let sumEven = 0;
+  for (let i = 0; i < arrayEven.length; i++) {
+    if (arrayEven[i] % 2 == 0) {
+      sumEven += arrayEven[i];
+    } else {
+      sumEven += 0;
+    }
+  }
+  return sumEven;
+};
+console.log(numberSumEven(numbers6));
+
+//задание 6.3,6.4
+const numberMax = (arrayMax) => {
+  let max = arrayMax[0];
+  let indexMax = [];
+  for (let i = 0; i < arrayMax.length; i++) {
+    if (max >= arrayMax[i]) {
+      max;
+    } else {
+      max = arrayMax[i];
+    }
+  }
+  for (let i = 0; i < arrayMax.length; i++) {
+    if (max == arrayMax[i]) {
+      indexMax.push(i);
+    }
+  }
+
+  console.log(`максимальное число:${max},индекс элементов массива:${indexMax}`);
+  return [max, indexMax];
+};
+
+numberMax(numbers6);
+
+// Задание 7
+
+// Определить массив let arr = [5, 4, 3, -3, -10, -1, 8, -20, 0]
+
+// В цикле создать новый массив из элементов массива arr, но в новом должны содержаться только положительные элементы.
+
+// Подсказка. Изначально нужно объявить пустой массив-хранилище (например, let newArr = []). Затем пройтись в цикле по массиву arr, и в каждой итерации, если элемент положительный, добавлять его в массив newArr.
+
+let arr = [5, 4, 3, -3, -10, -1, 8, -20, 0];
+let newArr = [];
+for(let i = 0; i < arr.length; i++){
+
+if(arr[i] > 0 ){
+    newArr.push(arr[i]);
+}
+}
+console.log(newArr);
+
+// Задание 8
+
+// Написать функцию, которая принимает массив (array) и число (num).
+
+// Функция должна создать новый массив из элементов массива array, и в этом новом массиве должны содержаться только элементы, больше и равные (>=) значению num.
+
+let numbers8 = [100,50,7,10,12,16,32,44,443,48,31,32,64,53,22];
+const numberNum =  (array, num) =>{
+    let arrayNum = [];
+    for(let i = 0; i < array.length; i++){
+        if(array[i] >= num){
+arrayNum.push(array[i]);
+
+        }
+    }
+    return arrayNum;
+
+}
+console.log(numberNum(numbers8,100));
+
+// Задание 9
+
+// Существует массив польщователей, например:
+
+// const users = [{name: 'Vasya', age: 23}, {name: 'Olya', age: 12}, {name: 'Anna', age: 22}, {name: 'Alex', age: 18}, {name: 'Valery', age: 8}]
+
+// Пройти в цикле по массиву и вывести имена всех пользователей, возраст которых больше 15.
+
+const users = [
+  { name: "Vasya", age: 23 },
+  { name: "Olya", age: 12 },
+  { name: "Anna", age: 22 },
+  { name: "Alex", age: 18 },
+  { name: "Valery", age: 8 },
+];
+for (let i = 0; i < users.length; i++) {
+if(users[i].age > 15){
+    console.log(users[i].name);
+}
+
+}
+
+// Задание 10*
+
+// Задать массив слов. Например:
+
+// let vegetables = ["морковь", "баклажан", "репа", "топинамбур"];
+
+// 1) В цикле сформировать массив объектов с ключами word (само слово), length (длина слова):
+
+// [{word:'морковь', length: 7}, {word:'баклажан', length: 8} и т.п.]
+
+// Вывести этот массив в консоль.
+
+// Подсказка: длину строки можно определить с помощью метода .length. Например, для переменной let string = 'text' это будет string.length (результат - 4, т.к. слово text состоит из 4 символов)
+
+// 2) Пройтись по полученному массиву объектов и вывести в консоль строки вида "слово - длина_слова", например "картошка - 8"
+
+let vegetables = ["морковь", "баклажан", "репа", "топинамбур"];
+let produce = [];
+for (let i = 0; i < vegetables.length; i++) {
+  let object = { word: vegetables[i], length: vegetables[i].length };
+  produce.push(object);
+}
+console.log(produce);
+
+for(let i = 0; i < produce.length;i++){
+   console.log(`${produce[i].word} - ${produce[i].length}`); 
+}
